233c97289fa70a64119160d66372654d0feba6e8
RiyaKumbhar <riyakumbhar2001@gmail.com>
2023-08-21 13:02:51 +0530

    Edited Exercise 4

--- HTML/Exercise4/script.js
+++ HTML/Exercise4/script.js
@@ -4,12 +4,15 @@ const user = {
     company: "Infuse Consulting",
     hobbies: ["Reading", "Listening to music", "Collecting stamps"]
 }
+let info = document.getElementById('info')
 
 const printUserProfile = () => {
     const { name, designation, company, hobbies } = user;
     const hobbiesList = hobbies.join(', ');
 
+
     console.log(`${name} is a ${designation} at ${company}. He likes ${hobbiesList}`);
+    info.innerHTML = `${name} is a ${designation} at ${company}. He likes ${hobbiesList}`
 }
 
 printUserProfile();
