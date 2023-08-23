0389987c8138c75d05ab705ccf607e8149c4be67
RiyaKumbhar <riyakumbhar2001@gmail.com>
2023-08-24 00:59:06 +0530

    Corrected Ex12

--- Selenium/Ex12.rb
+++ Selenium/Ex12.rb
@@ -1,31 +1,69 @@
 require 'selenium-webdriver'
 
-Selenium::WebDriver::Chrome::Service.driver_path = 'C:\Users\adity\Desktop\Drivers\chromedriver-win64\chromedriver-win64\chromedriver.exe'
-driver = Selenium::WebDriver.for :chrome
+class MyntraTest
+  def initialize
+    Selenium::WebDriver::Chrome::Service.driver_path = 'C:\Users\adity\Desktop\Drivers\chromedriver-win64\chromedriver-win64\chromedriver.exe'
+    @driver = Selenium::WebDriver.for :chrome
+    @wait = Selenium::WebDriver::Wait.new(timeout: 10)
+  end
 
-driver.get "https://www.myntra.com/"
+  def open_myntra
+    @driver.get "https://www.myntra.com/"
+  end
 
-search_input = driver.find_element(class: 'desktop-searchBar')
-search_input.send_keys('shoes')
-search_input.send_keys(:return)
+  def maximize_browser_window
+    @driver.manage.window.maximize
+  end
 
-wait = Selenium::WebDriver::Wait.new(timeout: 10)
-wait.until { driver.find_element(class: 'product-base') }
+  def search_for_product(product_name)
+    search_input = @driver.find_element(class: 'desktop-searchBar')
+    search_input.send_keys(product_name)
+    search_input.send_keys(:return)
+  end
 
-first_product = driver.find_element(class: 'product-base')
-first_product.click
+  def click_first_product
+    @wait.until { @driver.find_element(class: 'product-base') }
+    first_product = @driver.find_element(class: 'product-base')
+    first_product.click
+  end
 
-driver.switch_to.window(driver.window_handles.last)
+  def switch_to_new_tab
+    @driver.switch_to.window(@driver.window_handles.last)
+  end
 
-driver.execute_script('window.scrollBy(0, 300);')
+  def scroll_down(amount)
+    @driver.execute_script("window.scrollBy(0, #{amount});")
+  end
 
-product_title = driver.find_element(class: 'pdp-title')
-puts "Product Title: #{product_title.text}"
+  def get_product_title
+    product_title = @driver.find_element(class: 'pdp-title')
+    product_title.text
+  end
 
-product_price = driver.find_element(class: 'pdp-price')
-puts "Product Price: #{product_price.text}"
+  def get_product_price
+    product_price = @driver.find_element(class: 'pdp-price')
+    product_price.text
+  end
 
-open_pofile = driver.find_element(xpath: '//div[2]/div[2]/div/div[1]/span[1]')
-open_pofile.click
+  def close_browser
+    @driver.quit
+  end
 
-driver.quit
+  def perform_test_flow(product_name, scroll_amount)
+    open_myntra
+    maximize_browser_window
+    search_for_product(product_name)
+    click_first_product
+    switch_to_new_tab
+    scroll_down(scroll_amount)
+    title = get_product_title
+    price = get_product_price
+    close_browser
+
+    puts "Product Title: #{title}"
+    puts "Product Price: #{price}"
+  end
+end
+
+test = MyntraTest.new
+test.perform_test_flow('dresses', 300)
