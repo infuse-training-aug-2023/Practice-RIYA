048a11663e5a33e92f26056ddd49bd4da1a46733
RiyaKumbhar <riyakumbhar2001@gmail.com>
2023-08-23 23:57:23 +0530

    Corrected Exercise 4

--- Ruby/Exercise4/Exercise4.rb
+++ Ruby/Exercise4/Exercise4.rb
@@ -1,10 +1,19 @@
 class Sports
-    def initialize(arr)
-      @arr = arr
-    end
-  
-    def skip_sports(del)
-      @arr[del..-1]
-    end
+  def initialize(arr)
+    @arr = arr
   end
-  
+
+  def skip_sports(del)
+    validate_input(del)
+    skipped_sports = @arr[del..-1]
+    formatted_output = skipped_sports.each_with_index.map { |sport, index| "#{del + index} : #{sport}" }
+    formatted_output
+  end
+
+  private
+
+  def validate_input(del)
+    raise ArgumentError, "Invalid input: #{del}" unless del.is_a?(Integer) && del >= 0 && del < @arr.length
+  end
+end
+
--- Ruby/Exercise4/unitTest4.rb
+++ Ruby/Exercise4/unitTest4.rb
@@ -9,6 +9,16 @@ class TestSports < Test::Unit::TestCase
 
   def test_skip_sports
     result = @sports.skip_sports(2)
-    assert_equal(['football', 'hockey'], result)
+    assert_equal(["2 : football", "3 : hockey"], result)
+
+    result1 = @sports.skip_sports(3)
+    assert_equal(["3 : hockey"], result1)
+  end
+  
+
+  def test_skip_sports_with_invalid_input
+    assert_raise(ArgumentError) { @sports.skip_sports(-1) }
+    assert_raise(ArgumentError) { @sports.skip_sports(5) }
+    assert_raise(ArgumentError) { @sports.skip_sports('invalid') }
   end
 end
