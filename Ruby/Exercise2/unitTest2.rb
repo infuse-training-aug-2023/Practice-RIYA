ced8f9c2d4a2eb93ad131939499194f88b9c3e26
RiyaKumbhar <riyakumbhar2001@gmail.com>
2023-08-23 16:21:12 +0530

    Exercise 2 corrected

--- Ruby/Exercise2/unitTest2.rb
+++ Ruby/Exercise2/unitTest2.rb
@@ -22,4 +22,30 @@ class TestArrayManipulator < Test::Unit::TestCase
   def test_start_and_length
     assert_equal [2, 3, 4, 5], @manipulator.start_and_length(1, 4)
   end
+
+  def test_out_of_bounds_element_at
+    assert_nil @manipulator.element_at(-1)
+    assert_nil @manipulator.element_at(10)
+    assert_nil @manipulator.element_at(100)
+  end
+
+  def test_out_of_bounds_inclusive_range
+    assert_empty @manipulator.inclusive_range(8, 5)
+    assert_empty @manipulator.inclusive_range(-1, 3)
+    assert_empty @manipulator.inclusive_range(15, 20)
+  end
+
+  def test_out_of_bounds_non_inclusive_range
+    assert_empty @manipulator.non_inclusive_range(5, 3)
+    assert_empty @manipulator.non_inclusive_range(-2, 2)
+    assert_empty @manipulator.non_inclusive_range(10, 15)
+  end
+
+  def test_out_of_bounds_start_and_length
+    assert_empty @manipulator.start_and_length(9, 2)
+    assert_empty @manipulator.start_and_length(-1, 3)
+    assert_empty @manipulator.start_and_length(5, 10)
+  end
+
+
 end
