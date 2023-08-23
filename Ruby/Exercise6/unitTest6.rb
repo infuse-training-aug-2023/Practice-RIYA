e6cb5d97b09af202531bec471a44686c02d5dabd
RiyaKumbhar <riyakumbhar2001@gmail.com>
2023-08-23 23:55:15 +0530

    Corrected Exercise 6

--- Ruby/Exercise6/unitTest6.rb
+++ Ruby/Exercise6/unitTest6.rb
@@ -1,40 +1,38 @@
 require 'test/unit'
 require 'base64'
-require_relative 'Exercise6.2'
+require_relative 'Exercise6'
 
 class TestBase64Processor < Test::Unit::TestCase
-  def setup
-    @input_filename = 'input_test.txt'
-    @output_filename = 'output_test.txt'
-    @name = 'RiyaKumbhar'
-
-    @processor = Base64Processor.new(@input_filename, @output_filename, @name)
-  end
-
-  def teardown
-    File.delete(@input_filename) if File.exist?(@input_filename)
-    File.delete(@output_filename) if File.exist?(@output_filename)
-  end
-
-  def test_encode_bracketed_string
-    input_content = 'Originally, [John Doe] was a sham [string]...'
-    expected_output = 'Originally, [Sm9obiBEb2U=] was a sham [c3RyaW5n]...'
-
-    encoded_content = @processor.send(:encode_bracketed_string, input_content)
-    assert_equal(expected_output, encoded_content)
-  end
-
   def test_read_and_encode
-    input_content = 'Originally, [John Doe] was a sham [string]...'
-    expected_encoded_content = 'Originally, [Sm9obiBEb2U=] was a sham [c3RyaW5n]...'
+    input_content = "[Hello], [World!]"
+    expected_encoded_content = "[#{Base64.strict_encode64('Hello')}], [#{Base64.strict_encode64('World!')}]"
 
-    File.write(@input_filename, input_content)
+    input_content1 = "My name is [Riya]"
+    expected_encoded_content1 = "My name is [#{Base64.strict_encode64('Riya')}]"
 
-    @processor.read_and_encode
-    output_content = File.read(@output_filename)
+    # Create a temporary input file with the input content
+    input_file = 'temp_input.txt'
+    File.write(input_file, input_content)
 
-    assert_equal(expected_encoded_content, output_content)
+    # Create an instance of Base64Processor and call read_and_encode
+    output_file = 'temp_output.txt'
+    processor = Base64Processor.new(input_file, output_file)
+    processor.read_and_encode
+
+    actual_encoded_content = File.read(output_file)
+
+    assert_equal(expected_encoded_content, actual_encoded_content)
+
+  input_file1 = 'temp_input1.txt'
+  File.write(input_file1, input_content1)
+
+  output_file1 = 'temp_output1.txt'
+  processor = Base64Processor.new(input_file1, output_file1)
+  processor.read_and_encode
+
+  actual_encoded_content1 = File.read(output_file1)
+
+  assert_equal(expected_encoded_content1, actual_encoded_content1)
+  
   end
-
 end
-
