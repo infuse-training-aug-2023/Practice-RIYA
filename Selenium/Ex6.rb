121282ea64c740806598108841cfaa2f5010ad87
RiyaKumbhar <riyakumbhar2001@gmail.com>
2023-08-24 13:37:15 +0530

    Edited Exercise 6

--- Selenium/Ex6.rb
+++ Selenium/Ex6.rb
@@ -6,7 +6,6 @@ driver = Selenium::WebDriver.for :chrome
 driver.get 'https://letcode.in/forms'
 
 dropdown_element = driver.find_element(:xpath, '/html/body/app-root/app-forms/section[1]/div/div/div[1]/div/div/form/div[5]/div[2]/div/div/div/select')
-
 dropdown = Selenium::WebDriver::Support::Select.new(dropdown_element)
 
 dropdown.options.each do |option|
