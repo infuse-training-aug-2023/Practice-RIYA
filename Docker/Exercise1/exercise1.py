87d3f8ec139ef468483e2bb901ae0be0305e6f77
RiyaKumbhar <riyakumbhar2001@gmail.com>
2023-08-25 11:22:27 +0530

    Exercise 1 completed

---
+++ Docker/Exercise1/exercise1.py
@@ -0,0 +1,39 @@
+import numpy as np
+import os
+import sys
+
+# Set environment variables
+os.environ['VAR1'] = 'value1'
+os.environ['VAR2'] = 'value2'
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <arg1> <arg2>")
+        return
+    
+    arg1 = float(sys.argv[1])
+    arg2 = float(sys.argv[2])
+    
+    arr1 = np.arange(4, dtype=np.float_).reshape(2, 2)
+
+    print('First array:')
+    print(arr1)
+
+    print('\nSecond array:')
+    arr2 = np.array([arg1, arg2])
+    print(arr2)
+
+    print('\nAdding the two arrays:')
+    print(np.add(arr1, arr2))
+
+    print('\nSubtracting the two arrays:')
+    print(np.subtract(arr1, arr2))
+
+    print('\nMultiplying the two arrays:')
+    print(np.multiply(arr1, arr2))
+
+    print('\nDividing the two arrays:')
+    print(np.divide(arr1, arr2))
+
+if __name__ == "__main__":
+    main()
